868607f34248e6f9a8dc16dce18146fb6d4c61b9
Atrilos <101889118+Atrilos@users.noreply.github.com>
2023-04-23 00:22:06 +0700

    Final fixes

--- src/main/java/ru/skypro/homework/repository/CommentRepository.java
+++ src/main/java/ru/skypro/homework/repository/CommentRepository.java
@@ -7,13 +7,14 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.skypro.homework.model.Comment;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("select c from Comment c join fetch c.ads join fetch c.user where c.ads.id = ?1")
-    Optional<Comment> findAllByAdsId(Long adsId);
+    List<Comment> findAllByAdsId(Long adsId);
 
     @Query("delete from Comment c where c.id = ?1")
     @Transactional
